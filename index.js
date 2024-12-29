1847d15514e712484cc1ff0efa45d3d9b137fbab
Kat <kat.m.deans@gmail.com>
2024-12-29 14:45:52 -0500

    Contact form - testing

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 // template_1m5maw7
 // service_a317dbs
 // public key, user id:    zoYiX0Ta0E9JsH9IP
+
+
+function contact (event) {
+    event.preventDefault();
+    ElementInternalsjs
+        .sendForm(
+            'service_a317dbs',
+            'template_1m5maw7',
+            event.target,
+            'zoYiX0Ta0E9JsH9IP'
+     ).then(() => {
+        console.log('this worked1')
+     })
+    console.log('it worked')
+
+}
