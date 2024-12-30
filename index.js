8cb0da73007914ed519a21dad2ea2319e2547006
Kat <kat.m.deans@gmail.com>
2024-12-30 12:01:33 -0500

    Adding the form

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// template_1m5maw7
-// service_a317dbs
-// public key, user id:    zoYiX0Ta0E9JsH9IP
+// template_1m5maw7    new:  template_me72hl8
+// service_a317dbs    new:   service_t91zppx
+// public key, user id:    zoYiX0Ta0E9JsH9IP  new:   mjZvT58fzh8LQDid4
 
 
 function contact (event) {
     event.preventDefault();
     emailjs
         .sendForm(
-            'service_a317dbs',
-            'template_1m5maw7',
+            'seservice_t91zppx',
+            'template_me72hl8',
             event.target,
-            'zoYiX0Ta0E9JsH9IP'
+            'mjZvT58fzh8LQDid4'
      ).then(() => {
         console.log('this worked1')
      })
