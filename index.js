aec5a39b2d126ec658bb2ed90a40ce1c29fb5d05
Kat <kat.m.deans@gmail.com>
2024-12-30 12:22:09 -0500

    js for the form

--- index.js
+++ index.js
@@ -5,15 +5,27 @@
 
 function contact (event) {
     event.preventDefault();
-    emailjs
-        .sendForm(
-            'seservice_t91zppx',
-            'template_me72hl8',
-            event.target,
-            'mjZvT58fzh8LQDid4'
-     ).then(() => {
-        console.log('this worked1')
-     })
+    // emailjs
+    //     .sendForm(
+    //         'service_t91zppx',
+    //         'template_me72hl8',
+    //         event.target,
+    //         'mjZvT58fzh8LQDid4'
+    //  ).then(() => {
+    //     console.log('this worked1')
+    //  })
+
+     const loading = document.querySelector('.modal__overlay--loading')
+     const success = document.querySelector('.modal__overlay--success')
+     loading.classList += " modal__overlay--visible";
+// the space is very important before the modal__overlay as it is a list of classes, when you add a class we need a space 
+     setTimeout(() => {
+        loading.classList.remove("modal__overlay--visible");
+        success.classList += " modal__overlay--visible";
+         console.log('it worked 2')
+     }, 1000);
+ 
+
 
 
 }
